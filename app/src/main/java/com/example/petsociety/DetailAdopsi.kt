f26abf8a8cb1919574b2c5ea803f031bd2b0dc67
Irsyad M <irsyad.m2015@gmail.com>
2020-05-20 10:03:35 +0700

    ngerapihin sedikit

--- app/src/main/java/com/example/petsociety/DetailAdopsi.kt
+++ app/src/main/java/com/example/petsociety/DetailAdopsi.kt
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 
 class DetailAdopsi : AppCompatActivity() {
-    companion object{
+    companion object {
         const val EXTRA_JUDUL = "extra_judul"
         const val EXTRA_DESC = "extra_desc"
         const val EXTRA_TELP = "extra_telp"
@@ -17,16 +17,20 @@ class DetailAdopsi : AppCompatActivity() {
         const val EXTRA_JENKEL = "extra_jenkel"
         const val EXTRA_HEWAN = "extra_hewan"
     }
+
     private lateinit var pesan: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_adopsi)
 
-        val imgPhoto : ImageView = findViewById(R.id.img_item_adopsi)
+        val actionbar = supportActionBar
+        actionbar!!.setDisplayHomeAsUpEnabled(true)
+
+        val imgPhoto: ImageView = findViewById(R.id.img_item_adopsi)
         val tvJudul: TextView = findViewById(R.id.et_judul)
-        val tvDesk : TextView = findViewById(R.id.item_desk)
-        val tvJenkel : TextView = findViewById(R.id.jenis_kelamin_hewan)
-        val tvHewan : TextView = findViewById(R.id.jenis_hewan)
+        val tvDesk: TextView = findViewById(R.id.item_desk)
+        val tvJenkel: TextView = findViewById(R.id.jenis_kelamin_hewan)
+        val tvHewan: TextView = findViewById(R.id.jenis_hewan)
         val btn: Button = findViewById(R.id.btn_adopt)
 
         val judul = intent.getStringExtra(EXTRA_JUDUL)
@@ -36,10 +40,10 @@ class DetailAdopsi : AppCompatActivity() {
         val jenKel = intent.getStringExtra(EXTRA_JENKEL)
         val telp = intent.getStringExtra(EXTRA_TELP)
 
-        tvJudul.text=judul
-        tvHewan.text=hewan
-        tvJenkel.text=jenKel
-        tvDesk.text=desk
+        tvJudul.text = judul
+        tvHewan.text = hewan
+        tvJenkel.text = jenKel
+        tvDesk.text = desk
 
         Glide.with(this)
             .load(img)
@@ -50,9 +54,14 @@ class DetailAdopsi : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_SEND)
             intent.setType("text/plain")
             intent.putExtra(Intent.EXTRA_TEXT, pesan)
-            intent.putExtra("jid", "62"+telp+"@s.whatsapp.net")
+            intent.putExtra("jid", "62" + telp + "@s.whatsapp.net")
             intent.setPackage("com.whatsapp")
             startActivity(intent)
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
--- app/src/main/java/com/example/petsociety/detailDonasi.kt
+++ app/src/main/java/com/example/petsociety/detailDonasi.kt
@@ -9,7 +9,7 @@ import android.widget.TextView
 import com.bumptech.glide.Glide
 
 class detailDonasi : AppCompatActivity() {
-    companion object{
+    companion object {
         const val EXTRA_JUDUL = "extra_judul"
         const val EXTRA_DESC = "extra_desc"
         const val EXTRA_PHOTO = "extra_photo"
@@ -19,17 +19,20 @@ class detailDonasi : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_donasi)
 
-        val btnDonasi : Button = findViewById(R.id.btn_galang_di_detail)
+        val actionbar = supportActionBar
+        actionbar!!.setDisplayHomeAsUpEnabled(true)
+
+        val btnDonasi: Button = findViewById(R.id.btn_galang_di_detail)
         val imgPhoto: ImageView = findViewById(R.id.img_item_adopsi)
         val tvJudul: TextView = findViewById(R.id.et_judul)
-        val tvDesk : TextView = findViewById(R.id.item_desk)
+        val tvDesk: TextView = findViewById(R.id.item_desk)
 
         val judul = intent.getStringExtra(EXTRA_JUDUL)
         val img = intent.getStringExtra(EXTRA_PHOTO)
         val desk = intent.getStringExtra(EXTRA_DESC)
 
         tvJudul.text = judul
-        tvDesk.text=desk
+        tvDesk.text = desk
         Glide.with(this)
             .load(img)
             .into(imgPhoto)
@@ -38,5 +41,10 @@ class detailDonasi : AppCompatActivity() {
             val moveToPay = Intent(this, PilihNominalDonasiActivity::class.java)
             startActivity(moveToPay)
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
